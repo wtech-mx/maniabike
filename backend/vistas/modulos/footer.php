f07c5f7867c67a60b1ae5f0bc6a53d57dad514e3
verplancken <42614785+verplancken@users.noreply.github.com>
2019-12-27 23:58:07 -0600

    backend

---
+++ backend/vistas/modulos/footer.php
@@ -0,0 +1,7 @@
+ <!-- main-footer -->
+<footer class="main-footer">
+
+<strong>Copyright &copy; 2019 <a href="#">WebTech</a>.</strong> Todos los derechos reservados.
+
+</footer>
+<!-- main-footer -->
