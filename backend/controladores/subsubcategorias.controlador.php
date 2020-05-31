43c8cdb659e23c337db9247576e4a5817931f4ad
verplancken <42614785+verplancken@users.noreply.github.com>
2020-05-30 23:47:32 -0500

    Revert "Revert "controlador/ modelo Subsubcategorias""
    This reverts commit d8c5ff564f6b043cc35e2eb2d9c8753d7fab6531.

--- backend/controladores/subsubcategorias.controlador.php
+++ backend/controladores/subsubcategorias.controlador.php
@@ -163,7 +163,7 @@ class ControladorSubCategorias2{
 				if($_POST["selActivarOferta"] == "oferta"){
 
 					$datos = array("subsubcategoria"=>$_POST["tituloSubCategoria2"],
-								   "idSubCategoria"=>$_POST["seleccionarCategoria"],
+								   "idSubCategoria"=>$_POST["seleccionarSubCategoria"],
 								   "ruta"=>$_POST["rutaSubCategoria2"],
 								   "estado"=> 1,
 								   "titulo"=>$_POST["tituloSubCategoria2"],
@@ -179,7 +179,7 @@ class ControladorSubCategorias2{
 				}else{
 
 					$datos = array("subsubcategoria"=>$_POST["tituloSubCategoria2"],
-								   "idSubCategoria"=>$_POST["seleccionarCategoria"],
+								   "idSubCategoria"=>$_POST["seleccionarSubCategoria"],
 								   "ruta"=>$_POST["rutaSubCategoria2"],
 								   "estado"=> 1,
 								   "titulo"=>$_POST["tituloSubCategoria2"],
