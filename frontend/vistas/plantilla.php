5266d708b46e76bbe91c56b533aabfe92f8b01ea
verplancken <42614785+verplancken@users.noreply.github.com>
2020-06-07 15:20:01 -0500

    facebook
    se arreglo el inicio de sesion en facebook

--- frontend/vistas/plantilla.php
+++ frontend/vistas/plantilla.php
@@ -336,6 +336,26 @@ https://developers.facebook.com/
 
 <script>
 
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '658525324666740',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v7.0'
+    });
+
+    FB.AppEvents.logPageView();
+
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
   /*=============================================
 	COMPARTIR EN FACEBOOK
 	https://developers.facebook.com/docs/
